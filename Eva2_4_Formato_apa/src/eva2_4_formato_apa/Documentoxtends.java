315078481582fb05d8db4c53cea7b2558a17bff7
TheFabix3512 <124299405+TheFabix3512@users.noreply.github.com>
2023-03-08 13:39:40 -0600

    Add files via upload

---
+++ Eva2_4_Formato_apa/src/eva2_4_formato_apa/Documentoxtends.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_4_formato_apa;
+
+/**
+ *
+ * @author Fabian b
+ */
+public class Documentoxtends extends Documeto {
+    private String libro;
+    private String articulo;
+    private String informe;
+    private String sitioweb;
+    
+   public Documentoxtends(){
+        
+    }
+
+    public String getLibro() {
+        return libro;
+    }
+
+    public void setLibro(String libro) {
+        this.libro = libro;
+    }
+
+    public String getArticulo() {
+        return articulo;
+    }
+
+    public void setArticulo(String articulo) {
+        this.articulo = articulo;
+    }
+
+    public String getInforme() {
+        return informe;
+    }
+
+    public void setInforme(String informe) {
+        this.informe = informe;
+    }
+
+    public String getSitioweb() {
+        return sitioweb;
+    }
+
+    public void setSitioweb(String sitioweb) {
+        this.sitioweb = sitioweb;
+    }
+}
+
+
+
+
+
+
