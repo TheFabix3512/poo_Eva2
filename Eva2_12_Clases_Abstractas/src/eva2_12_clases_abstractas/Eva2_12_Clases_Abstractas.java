7412aadf9806a684932a0a553fe760c1988ce2be
TheFabix3512 <124299405+TheFabix3512@users.noreply.github.com>
2023-03-22 15:48:18 -0600

    Add files via upload

---
+++ Eva2_12_Clases_Abstractas/src/eva2_12_clases_abstractas/Eva2_12_Clases_Abstractas.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_12_clases_abstractas;
+
+/**
+ *
+ * @author Fabian b
+ */
+public class Eva2_12_Clases_Abstractas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Prueva Prueva = new Prueva();
+        
+      //  PruevaAbstracta pruevaabs = new PruevaAbstracta();
+        
+        
+        
+    }
+    
+}
+class Prueva{
+    
+}
+abstract class PruevaAbstracta{
+    //No se puede instanciar 
+    //Si se puede Heredar 
+}
